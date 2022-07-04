dea4cb7101beac420a0cc372d32f9e6e6ac04a73
StevenVallarsa <orionsjv@gmail.com>
2022-07-04 16:25:32 -0400

    Added constructors and method names

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      "to be or not to be",
+      "all those memories will be lost forever like tears in the rain",
+      "pale blue dot",
+      "hungry like the wolf",
+      "we stand on the shoulders of giants",
+    ];
+    this.activePhrase = null;
+  }
+
+  startGame() {}
+
+  getRandomPhrase() {
+    return this.phrases[Math.random() * this.phrases.length];
+  }
+
+  handleInteraction() {}
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  //      <li class="hide letter w">w</li>
+  //      <li class="space"> </li>
+
+  addPhraseToDisplay() {
+    const ul = document.querySelector("#phrase ul");
+    for (let letter in this.phrase) {
+      if (letter === " ") {
+        ul.appendChild("<li class='space'> </li>");
+      } else {
+        ul.appendChild(`<li class="hide letter ${letter}">${letter}</li>`);
+      }
+    }
+  }
+
+  checkLetter() {
+    const letters = document.querySelectorAll("#phrase>li");
+  }
+
+  showMatchedLetters() {}
+}
